ba648a0e6265d7fc827ccae554676bc5ea42fed6
Ishika Knsal <ishikakasal05@gmail.com>
2025-03-23 23:41:39 +0530

    add

--- src/components/shared/Final.jsx
+++ src/components/shared/Final.jsx
@@ -4,7 +4,7 @@ const Final = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-black relative">
       <video
-        className="w-[60vw] h-auto"
+        className="w-[80vw] h-auto"
         autoPlay
         loop
         controls
@@ -13,14 +13,14 @@ const Final = () => {
         Your browser does not support the video tag.
       </video>
 
-          <p className="absolute bottom-5 left-5  text-white font-bold bg-purple-600 px-3 py-1 rounded-2xl">Harry Potter dekhle😭</p>
+          {/* <p className="absolute bottom-5 left-5  text-white font-bold bg-purple-600 px-3 py-1 rounded-2xl">Harry Potter dekhle😭</p>
       <div className="absolute bottom-4 right-4 flex items-center space-x-2 ">
         <img
           src="./fighting.gif" // Replace with your GIF URL
           alt="Harry Potter"
           className="w-40 h-40 transform scale-x-[-1]"
         />
-      </div>
+      </div> */}
     </div>
   );
 };
